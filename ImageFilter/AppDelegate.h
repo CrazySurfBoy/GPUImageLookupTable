695364acd61fc55468e140e94f3171fbe1c1e050
Jing <surfboy@SurfBoy-MacBook-Pro.local>
2016-01-16 23:04:07 +0800

    1.0

---
+++ ImageFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageFilter
+//
+//  Created by SurfBoy on 1/14/16.
+//  Copyright © 2016 CrazySurfboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+pod 'GPUImage', '~> 0.1.7'
